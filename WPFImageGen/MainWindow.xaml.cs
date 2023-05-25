13c2fdb0ade4c6fc19b0eb1fb780b55ac1a8eb9c
Ryan Appleyard <theryanappleyard@gmail.com>
2023-05-25 16:12:52 +0100

    Generates images up to 18x (v2), encodes without RS and no ability to download despite button.

--- WPFImageGen/MainWindow.xaml.cs
+++ WPFImageGen/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
 
 namespace WPFImageGen
 {
@@ -20,9 +22,12 @@ namespace WPFImageGen
     /// </summary>
     public partial class MainWindow : Window
     {
+        WriteableBitmap finalBitmap;
+
         public MainWindow()
         {
             InitializeComponent();
+            lblText.Content = "Pogo";
         }
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
@@ -31,48 +36,25 @@ namespace WPFImageGen
             ConvertToBinary(sample);
         }
 
-        private void SampleRect()
-        {
-            int width = 300;
-            int height = 300;
-
-            WriteableBitmap bitmap = new WriteableBitmap(width, height, 1, 1, PixelFormats.Bgra32, null);
-            uint[] pixels = new uint[width * height];
-
-            int red;
-            int green;
-            int blue;
-            int alpha;
-
-            for (int x = 0; x < width; ++x)
-            {
-                for (int y = 0; y < height; ++y)
-                {
-                    int i = width * y + x;
-
-                    red = 0;
-                    green = 255 * y / height;
-                    blue = 255 * (width - x) / width;
-                    alpha = 255;
-
-                    pixels[i] = (uint)((blue << 24) + (green << 16) + (red << 8) + alpha);
-                }
-            }
-            bitmap.WritePixels(new Int32Rect(0, 0, 2, 2), pixels, width * 4, 0);
-            this.MainImage.Source = bitmap;
-
-        }
-
         private void ConvertToBinary(string input)
         {
             lblText.Content = "Converting to binary.";
             StringBuilder binaryBuilder = new StringBuilder();
 
-            foreach(char c in input)
+            //detect for format. If all numbers then 6 bit.
+
+            int n;
+            bool isNumeric = int.TryParse(input, out n);
+            string nString = n.ToString();
+
+            foreach (char c in input)
             {
-                string binary = Convert.ToString(c, 2);//.PadLeft(8, '0');
+                string binary = Convert.ToString(c, 2).PadLeft(8, '0');
                 binaryBuilder.Append(binary);
             }
+            
+
+
             lblText.Content = binaryBuilder.ToString();
             EncodingToPairs(binaryBuilder.ToString());
         }
@@ -100,116 +82,67 @@ namespace WPFImageGen
             CreateCode(pairsArray);
         }
 
-        private void AssignPairsToRects (string[] pairs)
-        {
-            lblText.Content = "Assigning Pairs to Rects.";
-            int width = 200;
-            int height = 200;
-            double realWidth = this.ActualWidth;
-            double realHeight = this.ActualHeight;
-            WriteableBitmap bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
-
-            int[] pixelTest = new int[width * height];
-            int red;
-            int green;
-            int blue;
-            int alpha = 255;
-
-            for(int i =0; i < pairs.Length; i++)
-            {
-                //string pair = pairs[i];
-                int pairInt;
-                int.TryParse(pairs[i], out pairInt);
-
-                switch (pairInt)
-                {
-                    case 0:
-                        //make cyan rect
-                        red = 0;
-                        green = 255;
-                        blue = 255;
-                        pixelTest[i] = (blue << 24) + (green << 16) + (red << 8) + alpha;
-                        break;
-                    case 1:
-                        //make meganta rect
-                        red = 255;
-                        green = 0;
-                        blue = 255;
-                        pixelTest[i] = (blue << 24) + (green << 16) + (red << 8) + alpha;
-                        break;
-                    case 10:
-                        //make yellow rect
-                        red = 255;
-                        green = 255;
-                        blue = 0;
-                        pixelTest[i] = (blue << 24) + (green << 16) + (red << 8) + alpha;
-                        break;
-                    case 11:
-                        //make black rect
-                        red = 0;
-                        green = 0;
-                        blue = 0;
-                        pixelTest[i] = (red << 24) + (green << 16) + (blue << 8) + alpha;
-                        break;
-                }
-
-                lblText.Content = pixelTest[1].ToString();
-
-                //bitmap.WritePixels(new Int32Rect(0,0,(int)realWidth / 20, (int)realHeight / 20), pixelTest, width * 4, 0);
-                bitmap.WritePixels(new Int32Rect(0, 0, 200, 200), pixelTest, width * 4, 0);
-
-            }
-            //need to upscale bitmap before rendering.
-            bitmap.Resize((int)bitmap.Width * 20, (int)bitmap.Height * 20, WriteableBitmapExtensions.Interpolation.NearestNeighbor);
-
-            WriteableBitmap newBitmap = new WriteableBitmap(200, 200, 96, 96, PixelFormats.Bgra32, null);
-            newBitmap.FillRectangle(0, 0, 40, 40, Colors.Red);
-            WriteableBitmap newBitmap1 = new WriteableBitmap(200, 200, 96, 96, PixelFormats.Bgra32, null);
-            newBitmap1.FillTriangle(0, 0, 40, 0, 40, 40, Colors.Yellow);
-
-            newBitmap.FillTriangle(40, 40, 60, 40, 60, 60, Colors.Crimson);
-
-            this.MainImage.Source = newBitmap;
-
-            lblText.Content = pairs.Length.ToString();
-            
-
-            
-            
-        }
-
         private void CreateCode(string[] pairs)
         {
             //check amount of pairs. If more than x amount, auto change size of code.
             //Also needs to go through more conditioning before the data can be used.
 
-
             int sizeMetric;
-            //if pairs.length >= 18(?) sizeMetric = 12
-            //?? = 18
-            //?? = 32
 
-            //replace all below definitions of size with variable int sizeMetric and equations to convert appropriately. I.E. int width = sizeMetric * 20;
-
-            int width = 240;
-            int height = 240;
-            WriteableBitmap bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
-            int sF = 20;//Scale Factor:: the multiplier for how much bigger we're making this.
-            float cS = 0.5f;//centre shift: amount from centre point to draw corners.
-
-            //define blocked off areas.
-
-            for(int i = 0; i < pairs.Length; i++)
+            switch (pairs.Length)
             {
-                int pairInt;
-                int.TryParse(pairs[i], out pairInt);
+                case < 64:
+                    sizeMetric = 12;
+                    break;
+                case >= 64 and < 184:
+                    //lblText.Content = "It Works.";
+                    sizeMetric = 18;
+                    break;
+                case >= 184:
+                    //lblText.Content = "Too big a boi.";
+                    sizeMetric = 32;
+                    break;
+                //need to cap
+            }
 
-                int row = i / 12;
-                int col = i % 12;
+            int width = sizeMetric * 20;
+            int height = sizeMetric * 20;
+            WriteableBitmap bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+            int sF = 20; //Scale Factor:: the multiplier for how much bigger we're making this.
+
+            for(int i = 0; i < (sizeMetric * sizeMetric); i++)
+            {
+                int row = i / sizeMetric;
+                int col = i % sizeMetric;
 
                 int rectX = col * sF;
                 int rectY = row * sF;
-                
+
+                bitmap.FillRectangle(rectX, rectY, rectX + sF, rectY + sF, Colors.Coral);
+            }
+
+            for(int i = 0; i < pairs.Length; i++)
+            {
+
+                int pairInt;
+                int.TryParse(pairs[i], out pairInt);
+
+                (int, int) coord = DefineDataSlots(i, sizeMetric);
+
+                //This allows wrapping on rows.
+                //int row = i / sizeMetric;
+                //int col = i % sizeMetric;
+
+                //This does not. Why?
+                int row = coord.Item2;// / sizeMetric;
+                int col = coord.Item1;// % sizeMetric;
+
+                //lblText.Content = coord.Item1.ToString() + ", " + coord.Item2.ToString();
+
+                int rectX = col * sF;
+                int rectY = row * sF;
+
+                //i corresponds to position in coord array.
 
                 switch (pairInt)
                 {
@@ -228,10 +161,104 @@ namespace WPFImageGen
                 }
             }
 
+            //merge overlay and data bitmaps.
+
+            //this.OverlayImage.Source = CreateOverlay(sizeMetric);
+
             this.MainImage.Source = bitmap;
-            lblText.Content = "Noice.";
+            finalBitmap = bitmap;
         }
 
+        private (int,int) DefineDataSlots(int i, int sizeMetric)
+        {
+            //Assigned data slot
+            (int, int)[] coords12 = new (int, int)[] { (6, 3), (6, 4), (6, 5), (7, 5), (7, 4), (7, 3), (8, 3), (8, 4), (8, 5), (9, 5), (9, 4), (9, 3), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 7), (10, 8), (10, 9), (10, 10), (9, 10), (8, 10), (8, 9), (9, 9), (9, 8), (8, 8), (8, 7), (9, 7), (9, 6), (8, 6), (7, 6), (7, 7), (7, 8), (7, 9), (7, 10), (6, 10), (6, 9), (6, 8), (6, 7), (6, 6), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10), (4, 10), (4, 9), (4, 8), (4, 7), (4, 6), (3, 6), (3, 7), (3, 7), (3, 9), (3, 10), (2, 10), (2, 9), (2, 8), (2, 7), (2, 6), (1, 6), (1, 7), (1, 8), (1, 9) };
+            (int, int)[] coords18 = new (int, int)[] { (6, 1), (6, 2), (7, 2), (7, 1), (8, 1), (8, 2), (9, 2), (9, 1), (10, 1), (10, 2), (11, 2), (11, 1), (12, 1), (12, 2), (12, 3), (12, 4), (11, 4), (11, 3), (10, 3), (10, 4), (9, 4), (9, 3), (8, 3), (8, 4), (7, 4), (7, 3), (6, 3), (6, 4), (6, 5), (7, 5), (7, 6), (6, 6), (5, 6), (4, 6), (3, 6), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (8, 7), (8, 6), (8, 5), (9, 5), (9, 6), (9, 7), (10, 7), (10, 6), (10, 5), (11, 5), (11, 6), (11, 7), (12, 7), (12, 6), (12, 5), (13, 5), (14, 5), (15, 5), (16, 5), (16, 6), (15, 6), (14, 6), (13, 6), (13, 7), (14, 7), (15, 7), (16, 7), (16, 8), (16, 9), (15, 9), (15, 8), (14, 8), (14, 9), (13, 9), (13, 8), (12, 8), (12, 9), (11, 9), (11, 8), (10, 8), (10, 9), (9, 9), (9, 8), (8, 8), (8, 9), (7, 9), (7, 8), (6, 8), (5, 8), (4, 8), (3, 8), (3, 9), (4, 9), (5, 9), (6, 9), (6, 10), (5, 10), (4, 10), (3, 10), (3, 11), (4, 11), (5, 11), (6, 11), (7, 11), (7, 10), (8, 10), (8, 11), (9, 11), (9, 10), (10, 10), (10, 11), (11, 11), (11, 10), (12, 10), (12, 11), (13, 11), (13, 10), (14, 10), (14, 11), (15, 11), (15, 10), (16, 10), (16, 11), (16, 12), (16, 13), (15, 13), (15, 12), (14, 12), (14, 13), (13, 13), (13, 12), (12, 12), (12, 13), (11, 13), (11, 12), (10, 12), (10, 13), (9, 13), (9, 12), (8, 12), (8, 13), (7, 13), (7, 12), (6, 12), (6, 13), (5, 13), (5, 12), (4, 12), (3, 12), (3, 13), (4, 13), (4, 14), (4, 15), (4, 16), (5, 16), (5, 15), (5, 14), (6, 14), (6, 15), (6, 16), (7, 16), (7, 15), (7, 14), (8, 14), (8, 15), (8, 16), (9, 16), (9, 15), (9, 14), (10, 14), (10, 15), (10, 16), (11, 16), (11, 15), (11, 14), (12, 14), (12, 15), (12, 16), (13, 16), (13, 15), (13, 14), (14, 14), (14, 15), (14, 16) };
+            (int, int)[] coords32 = new (int, int)[] { (1, 1) };
+
+            lblText.Content = coords18.Length;
+
+            switch (sizeMetric)
+            {
+                case 12:
+                    return coords12[i];
+                    break;
+                case 18:
+                    return coords18[i];
+                    break;
+                case 32:
+                    return coords32[i];
+                    break;
+                default:
+                    return coords32[i];
+                    break;
+            }
+
+        }
+
+        private WriteableBitmap CreateOverlay(int sizeMetric)
+        {
+            int width = sizeMetric * 20;
+            int height = sizeMetric * 20;
+            int sF = 20; //scale factor again.
+            WriteableBitmap overlayBMP = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+
+            //Create the overlay graphics for tracking and calibration.
+            (int, int)[] overlay12 = new (int, int)[] { (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10), (0, 11), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0) };
+            (int, int)[] overlay18 = new (int, int)[] { (0, 0) };
+            (int, int)[] overlay32 = new (int, int)[] { (0, 0) };
+
+
+
+            (int, int)[] overlayFormat = overlay12;
+            switch (sizeMetric)
+            {
+                case 12:
+                    overlayFormat = overlay12;
+                    break;
+                case 18:
+                    overlayFormat = overlay18;
+                    break;
+                case 32:
+                    overlayFormat = overlay32;
+                    break;
+            }
+
+            for(int i = 0; i < overlayFormat.Length; i++)
+            {
+                int row = i / sizeMetric;
+                int col = i % sizeMetric;
+
+                int rectX = col * sF;
+                int rectY = row * sF;
+
+                overlayBMP.FillRectangle(rectX, rectY, rectX + sF, rectY + sF, Colors.OrangeRed);
+            }
+            return overlayBMP;
+        }
+
+        private bool CheckAgainstOverlay(int i, int sizeMetric)
+        {
+            int row = i / sizeMetric;
+            int col = i % sizeMetric;
+
+            var coords = new[] { (1, row), (col, 1), (col, 2), (col, 3) };
+
+            for(int j = 0; j < coords.Length; j++)
+            {
+                if((row, col) == coords[j])
+                {
+                    //lblText.Content = "Big nice.";
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            Image<Rgba32> image =  new Image<Rgba32>()
+        }
 
     }
 }
